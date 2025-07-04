8b7b23cf0ea050dfa323bfda1b79f1acc4b5854a
Hélio Nogueira <116118382+helionlf@users.noreply.github.com>
2025-07-04 14:13:02 -0300

    .

--- src/main/java/com/app/rh_api/security/JwtAuthFilter.java
+++ src/main/java/com/app/rh_api/security/JwtAuthFilter.java
@@ -36,6 +36,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
         throws ServletException, IOException {
 
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
